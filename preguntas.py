1170b2aea9bc5b853e793faaaed00e7fa5983382
emhernandezva <emhernandezva@unal.edu.co>
2022-10-17 16:40:11 -0500

    Pregunta 3.2

--- preguntas.py
+++ preguntas.py
@@ -10,6 +10,7 @@ selección de las n variables más relevantes usando una prueba f.
 # pylint: disable=unsubscriptable-object
 
 import pandas as pd
+import numpy as np
 
 
 def pregunta_01():
@@ -115,7 +116,7 @@ def pregunta_03():
     # Defina un diccionario de parámetros para el GridSearchCV. Se deben
     # considerar valores desde 1 hasta 11 regresores para el modelo
     param_grid = {
-        'selectkBest__k': np.arange(1,12,1),
+        'selectkBest__k': np.arange(1,12),
     }
 
     # Defina una instancia de GridSearchCV con el pipeline y el diccionario de
